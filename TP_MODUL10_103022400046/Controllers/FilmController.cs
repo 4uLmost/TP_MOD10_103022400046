dadb221d2ba6068f154453201805e729b937b5a8
4uLmost <muhammaddhiyaul273@gmail.com>
2026-05-03 02:29:32 +0700

    menambahkan api

---
+++ TP_MODUL10_103022400046/Controllers/FilmController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TP_MODUL10_103022400046.Models;
+
+namespace TP_MODUL10_103022400046.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        private static List<Film> _filmList = new List<Film>
+        {
+            new Film { Judul = "Inception", Sutradara = "Christopher Nolan", Tahun = "2010", Genre = "Sci-Fi", Rating = "9.0" },
+            new Film { Judul = "Interstellar", Sutradara = "Christopher Nolan", Tahun = "2014", Genre = "Sci-Fi", Rating = "8.7" },
+            new Film { Judul = "Parasite", Sutradara = "Bong Joon-ho", Tahun = "2019", Genre = "Thriller", Rating = "8.6" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Film> Get()
+        {
+            return _filmList;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Film> Get(int id)
+        {
+            if (id < 0 || id >= _filmList.Count)
+            {
+                return NotFound("Index film tidak ditemukan.");
+            }
+            return _filmList[id];
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Film newFilm)
+        {
+            _filmList.Add(newFilm);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0 || id >= _filmList.Count)
+            {
+                return NotFound("Index film tidak ditemukan.");
+            }
+            _filmList.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
--- TP_MODUL10_103022400046/obj/Debug/net10.0/TP_MODUL10_103022400046.AssemblyInfo.cs
+++ TP_MODUL10_103022400046/obj/Debug/net10.0/TP_MODUL10_103022400046.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL10_103022400046")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+554a5e5a22fdf1af55c3ce1387d61014b1a4559a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL10_103022400046")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL10_103022400046")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
